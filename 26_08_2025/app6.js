f104f42bbec659987962e31801663860f7ea6714
CarlosAlberto251003 <carloscastelan2003@gmail.com>
2025-08-31 21:24:55 -0600

    Correccion tarea

--- 26_08_2025/app6.js
+++ 26_08_2025/app6.js
@@ -1,29 +1,18 @@
-let operacion = prompt("Ingresa la operacion: ")
 let numero1 = parseFloat(prompt("Ingresa Numero 1: "));
 let numero2 = parseFloat(prompt("Ingresa Numero 2: "));
 
 
-switch (operacion){
-    case "suma":
-        let suma = (numero1 + numero2);
-        console.log("el resultado de la suma: "+suma);
-    break;    
-    case "resta":
-        let resta = (numero1 - numero2);
-        console.log("el resultado de la resta: "+resta);
-    break;
-    case "multiplicacion":
-        let multiplicacion =(numero1 * numero2);
-        console.log("el resultado de la multiplicacion: "+multiplicacion);
-    break;
-    case "division":
-        let division = (numero1/numero2);
-        console.log("el resultado de la division: "+division)
-    break;
-
-}
+let suma = numero1 + numero2;
+console.log(suma);
 
 
+let resta = numero1 - numero2;
+console.log(resta);
 
 
+let multiplicacion = numero1 * numero2;
+console.log(multiplicacion);
 
+
+let divicion = numero1 / numero2;
+console.log(divicion);
