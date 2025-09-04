58c269d12812bd2cab2320ff661d29071871ff96
CarlosAlberto251003 <carloscastelan2003@gmail.com>
2025-09-03 20:13:42 -0600

    "tarea de clase 28-08-2025"

--- 28-08-2025/app4.js
+++ 28-08-2025/app4.js
@@ -5,10 +5,19 @@ let num3 = parseInt(prompt("Ingresa el numero 3: "));
 if (num1 == num2 && num2== num3){
     console.log("Son iguales");
 }
-if (num1 <= num2 && num1 <= num3){
-    console.log("Numero 1 es menor: "+ num1);
-}else if (num2 <= num1 && num2 <= num3){
-    console.log("Numero 2 es menor: "+ num2);
+if (num1 >= num2 && num1 >= num3){
+    console.log("Numero 1 es mayor: "+ num1);
+}else if (num2 >= num1 && num2 >= num3){
+    console.log("Numero 2 es mayor: "+ num2);
 }else{
-    console.log("Numero 3 es menor: "+ num3);
+    console.log("Numero 3 es mayor: "+ num3);
+}
+
+
+if (num1 >= num2 && num1 >= num3){
+    console.log("Numero 1 es mayor: "+ num1);
+}else if (num2 >= num1 && num2 >= num3){
+    console.log("Numero 2 es mayor: "+ num2);
+}else{
+    console.log("Numero 3 es mayor: "+ num3);
 }
